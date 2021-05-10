9a7bfccec05f0554949622df64626ba6db1d5008
Androz2091 <androz2091@gmail.com>
2021-05-10 08:52:23 +0200

    :bug: Some fixes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## My Blender Scripts
 
-Simple scripts that I use to simplify my renders with Blender.
+Collection of utility scripts that I use to simplify my renders with Blender.
--- patch-missing-frames.js
+++ patch-missing-frames.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const files = fs.readdirSync('./input');
 const fileFormat = /(\d+)\.exr/;
-const sortedFiles = files.map((file) => parseInt(file.match(fileFormat)[1])).sort((a, b) => a - b);
+const sortedFiles = files.map((file) => parseInt(path.basename(file).match(fileFormat)[1])).sort((a, b) => a - b);
 for (let i = sortedFiles[0]; i < sortedFiles[sortedFiles.length - 1]; i++) {
     const fileName = i.toString().padStart(4, '0');
     const previousFileName = (i-1).toString().padStart(4, '0');
