e8709dc81f6121daa9fe3e398b88645a7681592e
Androz <androz2091@gmail.com>
2021-05-10 16:24:01 +0200

    🐛 Use only one codec

--- ffmpeg-exrs-to-mp4.js
+++ ffmpeg-exrs-to-mp4.js
@@ -14,6 +14,6 @@ const sortedOutFiles = outFiles.filter((file) => outFileFormat.test(file)).map((
 const maxOut = sortedOutFiles[sortedOutFiles.length-1] || 0;
 
 //const command = `ffmpeg -r 24  -f image2 -s 1920x1080 -start_number ${minFrame} -i input/%04d.exr -i sound.mp3 -vframes 1000 -vf curves=strong_contrast -vcodec libx264 -crf 25  -pix_fmt yuv420p output/${maxOut+1}.mp4`;
-const command = `ffmpeg -apply_trc iec61966_2_1 -r 24 -start_number ${minFrame} -i input/%04d.exr -i sound.mp3 -vcodec mpeg4 -c:v libx264 -crf 20 -c:a aac -strict -2 output/${maxOut+1}.mp4`;
+const command = `ffmpeg -apply_trc iec61966_2_1 -r 24 -start_number ${minFrame} -i input/%04d.exr -i sound.mp3 c:v libx264 -crf 20 -c:a aac -strict -2 output/${maxOut+1}.mp4`;
 console.log(`Running ${command}`);
 exec(command);
